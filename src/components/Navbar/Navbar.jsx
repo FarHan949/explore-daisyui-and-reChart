0e44a190d228f4ecfb1488da41fd475978dda672
FarHan949 <uddinfarhan949@gmail.com>
2024-09-09 21:50:07 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Navbar = () => {
+   const routes = [
+    {id : 1, path: '/', name: 'Home'},
+    {id : 2, path: '/about', name: 'About'},
+    {id : 3, path: '/services', name: 'Services'},
+    {id : 4, path: '*', name: 'notFounded'}
+   ]
+
+    return (
+        <nav>
+              
+        </nav>
+    );
+};
+
+export default Navbar;
